e300413b854d0418cda5fdeb9cf3eef866215a76
shmathew511 <shawn.mathew.1@stonybrook.edu>
2021-05-27 17:53:38 -0400

    didnt save in last commit. Previous commit comments apply here

--- models/foldit_model.py
+++ models/foldit_model.py
@@ -27,11 +27,11 @@ class FoldItModel(BaseModel):
         parser.set_defaults(no_dropout=True) 
 
         if is_train:
-            parser.add_argument('--lambda_adv', type=float, default=10.0, help='weight for adversarial portion')
+            parser.add_argument('--lambda_adv', type=float, default=1.0, help='weight for adversarial portion')
             parser.add_argument('--lambda_T', type=float, default=10.0, help='weight for transitve loss')
             parser.add_argument('--lambda_GT', type = float, default = 10.0, help = 'weight for ground truth loss')
 
-            parser.add_argument('--lambda_identity', type=float, default=0.5, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
+            parser.add_argument('--lambda_identity', type=float, default= 1.0, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
 
 
         return parser
@@ -46,7 +46,7 @@ class FoldItModel(BaseModel):
         """
         BaseModel.__init__(self, opt)
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
-        self.loss_names = ['G_A', 'G_B', 'G_AC', 'G_BC', 'cycle_AC', 'cycle_BC', 'pix', 'D_A','D_B', 'D_AC', 'D_BC','idt_AC','idt_BC','G_big','D_big']
+        self.loss_names = ['G_A', 'G_B', 'G_AC', 'G_BC', 'D_A','D_B', 'D_AC', 'D_BC','T_AC', 'T_BC','GT','idt_AC','idt_BC',]#'G_big','D_big']
 
 
         
@@ -54,10 +54,6 @@ class FoldItModel(BaseModel):
         visual_names_A = ['real_A', 'fake_B', 'rec_AC','fake_AC','idt_AC']
         visual_names_B = ['real_B', 'fake_A', 'rec_BC', 'fake_BC', 'real_C','idt_BC']
 
-        self.opt.lambda_identity = 0
-        if self.isTrain and self.opt.lambda_identity > 0.0:  # if identity loss is used, we also visualize idt_B=G_A(B) ad idt_A=G_A(B)
-            visual_names_A.append('idt_B')
-            visual_names_B.append('idt_A')
 
 
         self.visual_names = visual_names_A + visual_names_B  # combine visualizations for A and B
@@ -65,7 +61,7 @@ class FoldItModel(BaseModel):
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>.
         if self.isTrain:
 
-            self.model_names = ['G_A', 'G_B', 'G_AC', 'G_BC', 'D_A','D_B', 'D_BC', 'D_AC','D_big']
+            self.model_names = ['G_A', 'G_B', 'G_AC', 'G_BC', 'D_A','D_B', 'D_BC', 'D_AC']
 
         else:  # during test time, only load Gs
             self.model_names = ['G_A', 'G_B', 'G_AC', 'G_BC']
@@ -98,21 +94,21 @@ class FoldItModel(BaseModel):
             self.netD_AC = networks.define_D(opt.input_nc, opt.ndf, opt.netD,
                                             opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
 
-            self.netD_big = networks.define_D(opt.input_nc*2, opt.ndf, opt.netD,
-                                opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
+            # self.netD_big = networks.define_D(opt.input_nc*2, opt.ndf, opt.netD,
+            #                     opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
 
 
         if self.isTrain:
             if opt.lambda_identity > 0.0:  # only works when input and output images have the same number of channels
                 assert(opt.input_nc == opt.output_nc)
-            self.fake_A_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
-            self.fake_B_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
-            self.rec_A_pool = ImagePool(opt.pool_size)        #extended change HERE <-------------------------
+            # self.fake_A_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
+            # self.fake_B_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
+            # self.rec_A_pool = ImagePool(opt.pool_size)        #extended change HERE <-------------------------
 
 
-            self.AtoB_pool = ImagePool(opt.pool_size)        #extended change HERE <-------------------------
-            self.BtoA_pool = ImagePool(opt.pool_size)        #extended change HERE <-------------------------
-            self.real_pool = ImagePool(opt.pool_size)        #extended change HERE <-------------------------
+            # self.AtoB_pool = ImagePool(opt.pool_size)        #extended change HERE <-------------------------
+            # self.BtoA_pool = ImagePool(opt.pool_size)        #extended change HERE <-------------------------
+            # self.real_pool = ImagePool(opt.pool_size)        #extended change HERE <-------------------------
 
 
             # define loss functions
@@ -122,7 +118,7 @@ class FoldItModel(BaseModel):
             self.L1Loss = torch.nn.L1Loss()
             # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
             self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), self.netG_B.parameters(),self.netG_AC.parameters(),self.netG_BC.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
-            self.optimizer_D = torch.optim.Adam(itertools.chain(self.netD_big.parameters(),self.netD_A.parameters(), self.netD_B.parameters(),self.netD_BC.parameters(),self.netD_AC.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D = torch.optim.Adam(itertools.chain(self.netD_A.parameters(), self.netD_B.parameters(),self.netD_BC.parameters(),self.netD_AC.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
 
             self.optimizers.append(self.optimizer_G)
             self.optimizers.append(self.optimizer_D)
@@ -144,23 +140,19 @@ class FoldItModel(BaseModel):
 
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
-        self.fake_B = self.netG_A(self.real_A)  # G_A(A)
-        self.rec_AC = self.netG_BC(self.fake_B)   # G_B(G_A(A))
-        self.fake_AC = self.netG_AC(self.real_A)
+        self.fake_B = self.netG_A(self.real_A)      # G_A(A)
+        self.rec_AC = self.netG_BC(self.fake_B)     # G_BC(G_A(A))
+        self.fake_AC = self.netG_AC(self.real_A)    # G_AC(A)
 
 
-        self.fake_A = self.netG_B(self.real_B)  # G_B(B)
-        self.rec_BC = self.netG_AC(self.fake_A)   # G_A(G_B(B))
-        self.fake_BC = self.netG_BC(self.real_B)
+        self.fake_A = self.netG_B(self.real_B)      # G_B(B)
+        self.rec_BC = self.netG_AC(self.fake_A)     # G_AC(G_B(B))
+        self.fake_BC = self.netG_BC(self.real_B)    # G_BC(B)
 
-        self.idt_AC = self.netG_AC(self.real_C)
-        self.idt_BC = self.netG_BC(self.real_C)
+        self.idt_AC = self.netG_AC(self.real_C)     # G_AC(C)
+        self.idt_BC = self.netG_BC(self.real_C)     # G_BC(C)
 
 
-        # self.ex_fake_B = self.netG_A(self.rec_A) #G_A(G_B(G_A(A)))
-        # self.AtoB = torch.cat((self.real_A,self.fake_B),1)
-        # self.BtoA = torch.cat((self.fake_A,self.real_B),1)
-        #print(self.real_A.shape,self.AtoB.shape)
 
     def backward_D_basic(self, netD, real, fake):
         """Calculate GAN loss for the discriminator
@@ -187,13 +179,13 @@ class FoldItModel(BaseModel):
     def backward_D_A(self):
         """Calculate GAN loss for discriminator D_A"""
 
-        fake_B = self.fake_B_pool.query(self.fake_B)
+        # fake_B = self.fake_B_pool.query(self.fake_B)
         self.loss_D_A = self.backward_D_basic(self.netD_A, self.real_B, fake_B)
 
     def backward_D_B(self):
         """Calculate GAN loss for discriminator D_A"""
 
-        fake_A = self.fake_A_pool.query(self.fake_A)
+        # fake_A = self.fake_A_pool.query(self.fake_A)
         self.loss_D_B = self.backward_D_basic(self.netD_B, self.real_A, fake_A)
 
 
@@ -207,13 +199,13 @@ class FoldItModel(BaseModel):
         """Calculate GAN loss for discriminator D_BC"""
         self.loss_D_BC = self.backward_D_basic(self.netD_BC, self.real_C, self.fake_BC.detach())
 
-    def backward_D_big(self):
-        """Calculate GAN loss for discriminator D_A"""
+    # def backward_D_big(self):
+    #     """Calculate GAN loss for discriminator D_A"""
 
-        #conditional loss
-        real = torch.cat((self.real_B,self.real_C),1)
-        syn = torch.cat((self.fake_B.detach(),self.fake_AC.detach()),1)
-        self.loss_D_big = self.backward_D_basic(self.netD_big, real, syn)
+    #     #conditional loss
+    #     real = torch.cat((self.real_B,self.real_C),1)
+    #     syn = torch.cat((self.fake_B.detach(),self.fake_AC.detach()),1)
+    #     self.loss_D_big = self.backward_D_basic(self.netD_big, real, syn)
 
 
 
@@ -222,51 +214,55 @@ class FoldItModel(BaseModel):
 
 
         """Calculate the loss for generators G_A and G_B"""
-        lambda_idt = .1
-        lambda_A = self.opt.lambda_A
-        lambda_B = self.opt.lambda_B
-        # self.AtoB = torch.cat((self.real_A,self.fake_B),1)
-        # self.BtoA = torch.cat((self.fake_A,self.real_B),1)
 
+        #get weights
+        lambda_idt = self.opt.lambda_identity
+        lambda_T = self.opt.lambda_T
+        lambda_GT = self.opt.lambda_GT
+        lambda_adv = self.opt.lambda_adv
 
-        # GAN loss D_B(G_B(B))
-        self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), True)
-
-
-
-        self.loss_G_BC = self.criterionGAN(self.netD_BC(self.fake_BC),True)
-
-
-        self.loss_cycle_BC = self.criterionCycle(self.fake_BC, self.rec_BC) * lambda_B
-
-
-        self.loss_pix = self.L1Loss(self.fake_BC, self.real_C)
-
-        self.loss_idt_BC = self.criterionIdt(self.idt_BC, self.real_C) * lambda_B * lambda_idt
-
-
-
+        #______________________________________________________
+        #Adversarial Losses
+        #______________________________________________________
 
         # GAN loss D_A(G_A(A))
-        self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B), True)
+        self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B), True)* lambda_adv
 
+        # GAN loss D_AC(G_AC(A))
+        self.loss_G_AC = self.criterionGAN(self.netD_AC(self.fake_AC),True)* lambda_adv
 
         # GAN loss D_B(G_B(B))
-        self.loss_G_AC = self.criterionGAN(self.netD_AC(self.fake_AC),True)
+        self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), True) * lambda_adv
 
-        #conditional loss
- 
-        #Extended Cycle loss
-        self.loss_cycle_AC = self.criterionCycle(self.fake_AC, self.rec_AC) * lambda_A
-        # Backward cycle loss || G_A(G_B(B)) - B||
-        # self.loss_cycle_BC = self.criterionCycle(self.fake_BC, self.rec_BC) * lambda_B
+        # GAN loss D_BC(G_BC(B))
+        self.loss_G_BC = self.criterionGAN(self.netD_BC(self.fake_BC),True)* lambda_adv
 
 
-        self.loss_idt_AC = self.criterionIdt(self.idt_AC, self.real_C) * lambda_A * lambda_idt
+        #______________________________________________________
+        #Transitive Losses
+        #______________________________________________________
 
-        # syn = torch.cat((self.fake_B,self.fake_AC),1)
-        # self.loss_G_big  = self.criterionGAN(self.netD_big(syn),True)
+        self.loss_T_BC = self.criterionCycle(self.fake_BC, self.rec_BC) * lambda_T
 
+        self.loss_T_AC = self.criterionCycle(self.fake_AC, self.rec_AC) * lambda_T
+
+
+        #______________________________________________________
+        #Ground Truth Losses
+        #______________________________________________________
+
+        self.loss_GT = self.L1Loss(self.fake_BC, self.real_C) * lambda_GT 
+
+
+        #______________________________________________________
+        #Identity Losses
+        #______________________________________________________
+        self.loss_idt_BC = self.criterionIdt(self.idt_BC, self.real_C) * lambda_idt
+
+        self.loss_idt_AC = self.criterionIdt(self.idt_AC, self.real_C) * lambda_idt
+
+
+        #sum losses and do backwards operation
         self.loss_G = self.loss_G_B + self.loss_G_BC + self.loss_cycle_BC + self.loss_pix + self.loss_idt_BC + self.loss_G_A + self.loss_G_AC  + self.loss_cycle_AC + self.loss_idt_AC #+self.loss_G_big
         self.loss_G.backward()
 
@@ -293,7 +289,6 @@ class FoldItModel(BaseModel):
         # self.backward_D()      # calculate gradients for D_A
         self.backward_D_A()      # calculate gradients for D_A
         self.backward_D_B()      # calculate graidents for D_B
-        self.backward_D_big()
         self.optimizer_D.step()  # update D_A and D_B's weights
 
 

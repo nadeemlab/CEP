522355e63f89da557d436c212247666f137c46e5
shmathew511 <shawn.mathew.1@stonybrook.edu>
2022-11-15 16:46:11 -0500

    Update base_options.py
    Update ngf default parameter

--- options/base_options.py
+++ options/base_options.py
@@ -28,7 +28,7 @@ class BaseOptions():
         parser.add_argument('--model', type=str, default='foldit', help='chooses which model to use. [foldit]')
         parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels: 3 for RGB and 1 for grayscale')
         parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels: 3 for RGB and 1 for grayscale')
-        parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in the last conv layer')
+        parser.add_argument('--ngf', type=int, default=80, help='# of gen filters in the last conv layer')
         parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in the first conv layer')
         parser.add_argument('--netD', type=str, default='basic', help='specify discriminator architecture [basic | n_layers | pixel]. The basic model is a 70x70 PatchGAN. n_layers allows you to specify the layers in the discriminator')
         parser.add_argument('--netG', type=str, default='resnet_9blocks', help='specify generator architecture [resnet_9blocks | resnet_6blocks | unet_256 | unet_128]')
